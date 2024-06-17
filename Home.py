4677345f0ef2e6929097bbc2afcf9d618ff79d32
SP-SuryaDEV <123246343+SP-SuryaDEV@users.noreply.github.com>
2024-06-17 12:50:15 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,10 +3,12 @@ from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
 def isValidEmail(email):
-  return email.contains('@') and email.contains('.')
+  return email.find('@') > -1 and email.find('.') > -1
 
 with st.form(key='login'):
   email = st.text_input('Enter Authorized Email:', placeholder='Email')
 
   if email and not isValidEmail(email):
     st.error('Enter a Valid Email')
+
+  submit = st.form_submit_button('Authenticate')
