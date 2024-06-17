29182552199e06e78ac829a408d65e1b3c66acf7
SP-SuryaDEV <123246343+SP-SuryaDEV@users.noreply.github.com>
2024-06-17 15:27:17 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -43,9 +43,89 @@ def plotDataEditor(df):
 def evaluateChanges(df):
   return df[df['Select']][[col for col in df.columns if col != 'Select']]
 
+class Cacher:
+  @staticmethod
+  def newWorksheet(worksheet_name):
+    with open('worksheets.cache', 'a') as file:
+      file.append(f'{worksheet_name}, ')
+
+  @staticmethod
+  def readWorksheets():
+    worksheets = open('worksheets.cache', 'r').read().strip().split(', ')
+    return worksheets
+
+class Worksheet:
+  @staticmethod
+  def createNewWorksheet(worksheet_name, df):
+    st.session_state.conn.create(
+      worksheet=worksheet_name, 
+      data=df
+    )
+
+  @staticmethod
+  def updateWorksheet(worksheet_name, df):
+    st.session_state.conn.update(
+      worksheet=worksheet_name, 
+      data=df
+    )
+    
+@st.experimental_dialog('Commit Changes?')
+def commitChanges(df):
+  options = [
+    'Append To Verified',
+    'Create a New Worksheet',
+    'Append to Existing Worksheet'
+  ]
+
+  option_to_commit = st.selectbox(
+    label='Commit By Which Option',
+    options=options
+  )
+
+  if option_to_commit:
+    if option_to_commit == options[0]:
+      Worksheet.updateWorksheet(
+        'Verified',
+        pd.concat([getVerified(), df]).drop_duplicates()
+      )
+      
+    if option_to_commit == options[1]:
+
+      @st.experimental_dialog("Enter Name for New Worksheet")
+      def getWorksheetName():
+        worksheets = Cacher.readWorksheet()
+        
+        new_worksheet_name = st.text_input('Enter New Worksheet Name').strip()
+        submit = st.button('Create New Worksheet')
+
+        if submit:
+          if new_worksheet_name in worksheets:
+            st.error('Worksheet Already Exists... Enter Unique Name.')
+            return None
+          else:
+            return new_worksheet_name
+
+      new_worksheet_name = getWorksheetName()
+
+      if new_worksheet_name:
+        Worksheet.createNewWorksheet(new_worksheet_name, df)
+        
+    if option_to_commit == options[2]:
+      st.success('Hello')
+      
+
+def getResponses():
+  return preprocessSheet(st.session_state.conn.read(worksheet='Form responses 1', usecols = list(range(int(st.secrets.COLS))), ttl=150), 
+                                          select=True)
+
+def getVerified():
+  return preprocessSheet(st.session_state.conn.read(worksheet='Verified', usecols = list(range(int(st.secrets.COLS))), ttl=5))
+
 def isValidEmail(email):
   return email.find('@') > -1 and email.find('.') > -1
-  
+
+
+
 if not st.session_state.get('logged_in'):
   st.session_state.logged_in = False
   
@@ -73,14 +153,13 @@ else:
     if not st.session_state.get('conn'):
       establishSheetsConnections()
 
-    current_submissions = preprocessSheet(st.session_state.conn.read(worksheet='Form responses 1', usecols = list(range(int(st.secrets.COLS))), ttl=150), 
-                                          select=True)
-    verified = preprocessSheet(st.session_state.conn.read(worksheet='Verified', usecols = list(range(int(st.secrets.COLS))), ttl=150))
+    current_submissions = getResponses()
+    verified = getVerified()
 
-    st.write(':green[**Current Submissions**]')
+    st.write(':blue[### **Current Submissions**]')
     changes = plotDataEditor(current_submissions)
 
     st.dataframe(evaluateChanges(changes))
 
-    st.write(':red[**Verified**]')
+    st.write(':green[### **Verified**]')
     plotDataEditor(verified)
