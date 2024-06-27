a23154f670be8b0debac2314281b22654e51ad26
SP-SuryaDEV <123246343+SP-SuryaDEV@users.noreply.github.com>
2024-06-27 05:57:05 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
-# from streamlit_gsheets import GSheetsConnection
-# import pandas as pd
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
 import datetime
 import time
 import random
